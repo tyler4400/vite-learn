f202159d2ee9b736d755b038b7722c180100524f
tyler4400 <tyler4400@foxmail.com>
2021-06-27 20:47:07 +0800

    集成eslint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,41 @@
-import { createRouter, createWebHistory, } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-import Home from "../views/Home.vue" // vite来说，后缀.vue不可以省略
-import AnotherPage from "../views/AnotherPage.vue"
-
+import Home from '../views/Home.vue' // vite来说，后缀.vue不可以省略
+import AnotherPage from '../views/AnotherPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
-	{
-		path: '/',
-		name: '/',
-		redirect: '/Home'
-	},
-	{
-		path: '/Home',
-		name: 'Home',
-		component: Home,
-	},
-	{
-		path: '/AnotherPage',
-		name: 'AnotherPage',
-		component: AnotherPage
-	},
-	{
-		path: '/LazyLoadPage',
-		name: 'LazyLoadPage',
-		component: () => import('@/views/LazyLoadPage.vue') // 懒加载组件
-	},
+  {
+    path: '/',
+    name: '/',
+    redirect: '/Home',
+  },
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/AnotherPage',
+    name: 'AnotherPage',
+    component: AnotherPage,
+  },
+  {
+    path: '/LazyLoadPage',
+    name: 'LazyLoadPage',
+    // eslint-disable-next-line import/no-unresolved
+    component: () => import('@/views/LazyLoadPage.vue'), // 懒加载组件
+  },
   // 兜底
   {
     path: '/:pathMatch(.*)*',
     name: 'not-found',
     component: () => import('../components/NotFound.vue'),
-  }
+  },
 ]
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
+  history: createWebHistory(),
+  routes,
 })
 export default router
--- src/views/AnotherPage.vue
+++ src/views/AnotherPage.vue
@@ -10,12 +10,12 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 
-const fetchSomething = new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-      console.log('接口返回了');
-      resolve('返回成功')
-    }, 3000)
-  })
+const fetchSomething = new Promise<string>(resolve => {
+  setTimeout(() => {
+    console.log('接口返回了')
+    resolve('返回成功')
+  }, 3000)
+})
 /**
  * vue 异步处理_在Vue 3 / Composition API中处理异步
  * https://blog.csdn.net/weixin_26738395/article/details/108258992
@@ -24,24 +24,26 @@ const fetchSomething = new Promise<string>((resolve, reject) => {
  * <Suspense />
  * vue-promised
  */
-  export default defineComponent({
-    name: "AnotherPage",
-    setup() {
-      const isLoading = ref(false);
-      const error = ref(null);
-      const data = ref<string>('');
-      const getUsers = async () => {
-        isLoading.value = true;
-        data.value = await fetchSomething;
-        isLoading.value = false;
+export default defineComponent({
+  name: 'AnotherPage',
+  setup() {
+    const isLoading = ref(false)
+    const error = ref(null)
+    const data = ref<string>('')
+    const getUsers = async () => {
+      isLoading.value = true
+      data.value = await fetchSomething
+      isLoading.value = false
 
-        console.log('data', data.value);
-        console.log('isLoading', isLoading.value);
-      };
-
-      return { isLoading, error, data, getUsers };
+      console.log('data', data.value)
+      console.log('isLoading', isLoading.value)
     }
-  })
+
+    return {
+      isLoading, error, data, getUsers,
+    }
+  },
+})
 </script>
 
 <style scoped lang="scss">
