314cdc3732a80751a21ed7fed19cd0e32434b8bf
tyler4400 <tyler4400@users.noreply.github.com>
2021-06-29 12:24:32 +0000

    deploy to github pages 3118eca6398f74d8bee5ce3812e6c5427334b00a

---
+++ assets/NotFound.da4005ac.js
@@ -0,0 +1,1 @@
+import{o,c as e,w as a}from"./vendor.2fc6b0f5.js";const n={name:"NotFound"},r=a()(((a,n,r,d,s,t)=>(o(),e("p",null,"sorry, not found"))));n.render=r,n.__scopeId="data-v-007e44ae";export default n;
